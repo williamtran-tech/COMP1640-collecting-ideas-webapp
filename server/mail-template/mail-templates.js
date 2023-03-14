a273d4a8838db1d76be9252fb91307a8b30601af
williamtr4n <williamtran6111@gmail.com>
2023-03-14 13:19:58 +0700

    Mail letter

--- server/mail-template/mail-templates.js
+++ server/mail-template/mail-templates.js
@@ -18,16 +18,20 @@ exports.registration = (user, token) => {
     const html =`
     <html>
         <body style="color: black; margin-left: 20%; margin-right: 30%">
-        <img src="https://cdn.discordapp.com/attachments/1084789058305269773/1084789117461737482/black_logo.png" alt="logo" width="100">
-        <h2>Dear ${user.fullName},</h2> 
-        
-        <p>Thank you for choosing Gre Ideas! Welcome to your new and shiny Gre Ideas ID. You can use your new account to submit or contribute ideas and participate in the online Gre Ideas Community. Plus, it easy to update your account information at any time.</p>
-        
-        <p>To complete your Account set-up just click on the link below to confirm the email address associated with your Gre Ideas ID. The link will expire in 3 days, so please confirm as soon as possible.</p>
-        
-        <p>Username: ${user.email}</p>
-        <button style="background-color: rgb(0, 197, 0); border-radius: 10px; padding: 10px"><a href="http://localhost:5000/accounts/verify?token=${token}" style="text-decoration: none; color: #ffffff;"> Verify here</button>
+            <!-- this ensures Gmail doesn't trim the email -->
+            <span style="opacity: 0"></span>
+            <img src="https://cdn.discordapp.com/attachments/1084789058305269773/1084789117461737482/black_logo.png" alt="logo" width="100">
+            <h2>Dear ${user.fullName},</h2> 
+            
+            <p>Thank you for choosing Gre Ideas! Welcome to your new and shiny Gre Ideas ID. You can use your new account to submit or contribute ideas and participate in the online Gre Ideas Community. Plus, it easy to update your account information at any time.</p>
+            
+            <p>To complete your Account set-up just click on the link below to confirm the email address associated with your Gre Ideas ID. The link will expire in 3 days, so please confirm as soon as possible.</p>
+            
+            <p>Username: ${user.email}</p>
+            <button style="background-color: rgb(0, 197, 0); border-radius: 10px; padding: 10px"><a href="http://localhost:5000/accounts/verify?token=${token}" style="text-decoration: none; color: #ffffff;"> Verify here</button>
 
+            <!-- this ensures Gmail doesn't trim the email -->
+            <span style="opacity: 0"> ${Date.now()} </span>
         </body>
     </html>
     `
