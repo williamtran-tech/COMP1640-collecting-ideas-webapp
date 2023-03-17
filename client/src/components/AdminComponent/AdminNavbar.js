c5a5f2ec874abf2e463507f93795f05cb33bbdb8
Võ Hoàng Tâm <82395847+tamvo02@users.noreply.github.com>
2023-03-18 02:27:15 +0700

    update navbar in admin dashboard

--- client/src/components/AdminComponent/AdminNavbar.js
+++ client/src/components/AdminComponent/AdminNavbar.js
@@ -6,10 +6,13 @@ import TabPanel from '@mui/joy/TabPanel';
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.png'
 import { useState } from "react";
-import VisibilityIcon from '@mui/icons-material/Visibility';
+import TopicIcon from '@mui/icons-material/Topic';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import ListItemDecorator from '@mui/joy/ListItemDecorator';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import CategoryIcon from '@mui/icons-material/Category';
 import LogoutIcon from '@mui/icons-material/Logout';
+import ClassIcon from '@mui/icons-material/Class';
+import BarChartIcon from '@mui/icons-material/BarChart';
 import { useNavigate } from "react-router-dom";
 import '../../style/admin.css'
 const AdminNavbar = () => {
@@ -26,40 +29,34 @@ const handleChange = (event, newValue) => {
   return (
    <Box className="dashboard_navigation">
        <Grid className='admin-logo' xs={12}>
-            <img className ='logo' src={logo} alt="" />
-        </Grid>
-        <Divider></Divider>
-        <Grid className='admin-navigation' >
-           <div>
-             {/* <Typography variant="body2" className="Admin_function_name">Topic Management</Typography> */}
+            <div>
+                <img className ='logo_admin' src={logo} alt="" /> 
+            </div>
+            <div>
+          
              <Tabs orientation="vertical" 
              value={value}
              onChange={handleChange}
             >
-
               <TabList >
-                <Typography variant="body2" className="Admin_function_name">Topic Management</Typography>
-                <Tab variant={value === 1 ? 'solid' : 'plain'} color={value === 1 ? 'primary' : 'neutral'} iconPosition="start"className="tab" component={Link} to="/"> <ListItemDecorator><VisibilityIcon /></ListItemDecorator>  View all topic</Tab>
-                <Tab variant={value === 2 ? 'solid' : 'plain'} color={value === 2 ? 'primary' : 'neutral'} className="tab" component={Link} to="/users"><ListItemDecorator><AddCircleIcon /></ListItemDecorator>Create new topic</Tab>
-                <Typography variant="body2" className="Admin_function_name">Category Management</Typography>
-                <Tab variant={value === 4 ? 'solid' : 'plain'} color={value === 4 ? 'primary' : 'neutral'} className="tab" component={Link} to="/">View all topic</Tab>
-                <Tab variant={value === 5 ? 'solid' : 'plain'} color={value === 5 ? 'primary' : 'neutral'} className="tab" component={Link} to="/create">Create new topic</Tab>
-                <Typography variant="body2" className="Admin_function_name">User Management</Typography>
-                <Tab variant={value === 7 ? 'solid' : 'plain'} color={value === 7 ? 'primary' : 'neutral'} className="tab" component={Link} to="/">View all topic</Tab>
-                <Tab variant={value === 8 ? 'solid' : 'plain'} color={value === 8 ? 'primary' : 'neutral'} className="tab" component={Link} to="/create">Create new topic</Tab>
+                <Tab variant={value === 0 ? 'solid' : 'plain'} color={value === 0 ? 'primary' : 'neutral'} iconPosition="start" className="tab" component={Link} to="/"> <ListItemDecorator><TopicIcon /></ListItemDecorator>Topic Management</Tab>
+                <Tab variant={value === 1 ? 'solid' : 'plain'} color={value === 1 ? 'primary' : 'neutral'}  iconPosition="start" className="tab" component={Link} to="/users"><ListItemDecorator><AccountBoxIcon /></ListItemDecorator>User Management</Tab>
+                <Tab variant={value === 2 ? 'solid' : 'plain'} color={value === 2 ? 'primary' : 'neutral'} className="tab" component={Link} to="/"><ListItemDecorator><CategoryIcon /></ListItemDecorator>Category Management</Tab>
+                <Tab variant={value === 3 ? 'solid' : 'plain'} color={value === 3 ? 'primary' : 'neutral'} className="tab" component={Link} to="/"><ListItemDecorator><ClassIcon /></ListItemDecorator>Department Management</Tab>
+                <Tab variant={value === 4 ? 'solid' : 'plain'} color={value === 4 ? 'primary' : 'neutral'} className="tab" component={Link} to="/"><ListItemDecorator><BarChartIcon /></ListItemDecorator>Statistic</Tab>
                </TabList>
              </Tabs>
            </div>
-           <Divider></Divider>
-        </Grid>
-        <Grid className='admin-logo' xs={12}>
-          <IconButton onClick={logout}>
-              {/* <Badge badgeContent={4} color="error">
-                  <MailIcon className='icon'/>
-              </Badge> */}
-              <LogoutIcon className='icon'></LogoutIcon>
-          </IconButton>
-        </Grid>
+      </Grid>
+        <Divider></Divider>
+      <Grid className='admin_logout' xs={12} >
+        <IconButton onClick={logout}>
+            {/* <Badge badgeContent={4} color="error">
+                <MailIcon className='icon'/>
+            </Badge> */}
+            <LogoutIcon className='icon'></LogoutIcon>
+        </IconButton>
+      </Grid>
     </Box>
   )
 }
