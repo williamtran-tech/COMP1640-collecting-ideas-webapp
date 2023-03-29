333305121b754d4ffaad6783cf85d35b3587c0dc
Võ Hoàng Tâm <82395847+tamvo02@users.noreply.github.com>
2023-03-30 01:01:05 +0700

    update lilst topic filter by department

--- client/src/components/ListTopics.js
+++ client/src/components/ListTopics.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
 
-import { Box, CardHeader,Badge,IconButton, Chip, Typography } from '@mui/material'
+import { Box, CardHeader,Badge,IconButton, Chip, Typography, Stack } from '@mui/material'
 import { Card,CardActions, Divider} from '@mui/material'
 import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
@@ -13,6 +13,7 @@ import '../style/listitem.css'
 import Moment from "moment"
 import handleApi from '../service/handleApi'
 import { Link } from 'react-router-dom';
+import BusinessIcon from '@mui/icons-material/Business';
 const ListTopics = ({isLoggedIn}) => {
     const [listtopics, setlisttopics] = useState([]);
     const [getTopic, setGetTopic]=useState(true)
@@ -40,44 +41,46 @@ const ListTopics = ({isLoggedIn}) => {
                             </Typography>
                 </Grid>
             </Grid>
-        <Grid container className='list-item'>
-            {
-                listtopics.topics?.map((topic) =>
-                {console.log('closureDate:', Date(topic.closureDate));
-                console.log('finalClosureDate:', Date(topic.finalClosureDate));
-                console.log('now:', Date());
-                return(
-                <Grid item xs={12} md={4} className={"topic-item"}>
-                    <Link to = {`/topics/${topic.id}`} style={{ textDecoration: 'none'}}>
-                    <Card className={
-                       new Date(topic.finalClosureDate) < new Date() ? "topic_status_3":
-                       new Date(topic.closureDate) < new Date() ? "topic_status_2"
-                      :
-                      "topic_status_1"
-                    }>
-                    <CardHeader title={topic.name}  action={
-                        <IconButton>
-                            <Badge badgeContent={topic.idea_quantity} sx={{"& .MuiBadge-badge": {color: "#fefae0",backgroundColor: "#181D31"}}}>
-                                <EmojiObjectsIcon style={{ color: '#fefae0'}}/>
-                            </Badge>
-                        </IconButton>
-                    }>
-                    </CardHeader>
-                    <Divider />
-                    <CardActions>
-                        <Grid xs={12} className="date">
-                        <Chip icon={<AccessTimeIcon/>} label={Moment(topic.closureDate).format('YYYY/MM/DD')} sx={{backgroundColor: "#fefae0"}} />
-                        <Chip icon={<AccessTimeFilledIcon/>} label={Moment(topic.finalClosureDate).format('YYYY/MM/DD')} sx={{backgroundColor: "#fefae0"}} />
-                        </Grid>
-                    </CardActions>
-                </Card>
-              </Link>
-                
-            </Grid> 
-                )})
-            }
-        </Grid>
-      
+          {
+            listtopics.departments?.map((department)=>(
+              <>
+              <Stack direction={"row"} spacing={1} className='list-department' ><BusinessIcon></BusinessIcon><Typography variant='h5' fontWeight={"bolder"}>{department.name}</Typography></Stack>
+              
+              <Grid container className='list-item'>
+                { listtopics.topics?.filter(topic=> topic.Department.name  === department.name).map((topic) =>
+                    <Grid item xs={12} md={4} className={"topic-item"}>
+                        <Link to = {`/topics/${topic.id}`} style={{ textDecoration: 'none'}}>
+                        <Card className={
+                          new Date(topic.finalClosureDate) < new Date() ? "topic_status_3":
+                          new Date(topic.closureDate) < new Date() ? "topic_status_2"
+                          :
+                          "topic_status_1"
+                        }>
+                        <CardHeader title={topic.name}  action={
+                            <IconButton>
+                                <Badge badgeContent={topic.idea_quantity} sx={{"& .MuiBadge-badge": {color: "#fefae0",backgroundColor: "#181D31"}}}>
+                                    <EmojiObjectsIcon style={{ color: '#fefae0'}}/>
+                                </Badge>
+                            </IconButton>
+                        }>
+                        </CardHeader>
+                        <Divider />
+                        <CardActions>
+                            <Grid xs={12} className="date">
+                            <Chip icon={<AccessTimeIcon/>} label={Moment(topic.closureDate).format('YYYY/MM/DD')} sx={{backgroundColor: "#fefae0"}} />
+                            <Chip icon={<AccessTimeFilledIcon/>} label={Moment(topic.finalClosureDate).format('YYYY/MM/DD')} sx={{backgroundColor: "#fefae0"}} />
+                            </Grid>
+                        </CardActions>
+                    </Card>
+                  </Link>
+                </Grid> 
+              )}
+              </Grid>
+              <Divider></Divider>
+                </>
+              
+            ))
+          }
     </Box>
   )
 }
